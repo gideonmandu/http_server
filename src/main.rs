ecc65717497713855665152e87f7d68bea95ab0c
gideonmandu <gideonmandu@gmail.com>
2021-09-05 14:56:27 +0300

    Added path to public files

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@
 mod server;
 mod http;
 mod website_handler;
+use std::env;
 use server::Server;
 use website_handler::WebsiteHandler;
 
 
 fn main() {
-    println!("Server running 😡");
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("Server running 😡.");
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
